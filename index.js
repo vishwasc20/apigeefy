96a80fd8c9bdd17daa10e1fb02fce13f5d683b02
Vishwas C <vishwas.chandrashekar@tesco.com>
2023-04-14 19:01:24 +0530

    Fix apigeefy bin

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const browserify = require('browserify');
 const through = require('through2');
-
-function apigeefy (mod) {
+const babelTransform = require("@babel/core").transformSync;
+const uglifyJS = require("uglify-js");
+ 
+function apigeefyConvert (mod) {
   const replaceRegex = 'var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.apigeefy = f()'
   const policy = 'var g={};g.apigeefy=f();context.setVariable("bundle", g)'
 
-  var bopts = { standalone: 'apigeefy' };
+  const bopts = { standalone: 'apigeefy' };
 
   return browserify(mod, bopts)
    .bundle()
@@ -21,6 +23,29 @@ function apigeefy (mod) {
      }
      next();
    }));
+}
+
+function streamToString (stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  })
+}
+
+async function apigeefy (mod) {
+  const apigeefiedCode = await streamToString(apigeefyConvert(mod))
+  const transpiledCode = babelTransform(apigeefiedCode, {
+    plugins: ["@babel/plugin-transform-template-literals"],
+  });
+  const uglifiedCode = uglifyJS.minify(transpiledCode.code);
+  
+  if (uglifiedCode.error) {
+    throw new Error("Error transforming code");
+  }
+
+  process.stdout.write(uglifiedCode.code);
 }
 
 module.exports = apigeefy;
